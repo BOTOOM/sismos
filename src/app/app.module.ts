81a15c0a7c5550b38d85b03dea87461d9262506b
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-04-07 00:39:14 -0500

    eliminacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,36 +25,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BannerComponent } from './pages/home/banner/banner.component';
-import { ContactoComponent } from './pages/home/contacto/contacto.component';
-import { TopnewsComponent } from './pages/home/topnews/topnews.component';
-import { NewsComponent } from './pages/news/news.component';
-import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
-import { EventosComponent } from './pages/eventos/eventos.component';
-import { GruposDeInteresComponent } from './pages/grupos-de-interes/grupos-de-interes.component';
-import { TopnewsTarjetaComponent } from './pages/home/topnews/topnews-tarjeta/topnews-tarjeta.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NoticiaComponent } from './components/noticia/noticia.component';
-import { EventoComponent } from './components/evento/evento.component';
-import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent,
-    BannerComponent,
-    ContactoComponent,
-    TopnewsComponent,
-    NewsComponent,
-    QuienesSomosComponent,
-    EventosComponent,
-    GruposDeInteresComponent,
-    TopnewsTarjetaComponent,
-    NoticiaComponent,
-    EventoComponent,
-    ProyectosComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
